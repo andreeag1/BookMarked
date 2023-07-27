5127d2272e900ed36da9bceecb5ca1dfdd0dc817
andreeag1 <91742573+andreeag1@users.noreply.github.com>
2023-07-26 21:48:22 -0400

    configure database connection

--- server/src/lib/database.ts
+++ server/src/lib/database.ts
@@ -3,16 +3,32 @@ import { Book, User, Review } from "../entity";
 import { Comment } from "../entity/Comment";
 import { Collection } from "../entity/Collection";
 
-export const AppDataSource = new DataSource({
-  type: "mysql",
-  host: process.env.DB_HOST,
-  port: 3306,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  entities: [Book, User, Review, Comment, Collection],
-  synchronize: true,
-});
+export let AppDataSource: DataSource;
+
+if (process.env.ENV !== "prod") {
+  AppDataSource = new DataSource({
+    type: "mysql",
+    host: process.env.DB_HOST,
+    port: 3306,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [Book, User, Review, Comment, Collection],
+    synchronize: true,
+  });
+} else {
+  AppDataSource = new DataSource({
+    type: "mysql",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    extra: {
+      socketPath: process.env.DB_SOCKET_PATH,
+    },
+    entities: [Book, User, Review, Comment, Collection],
+    synchronize: true,
+  });
+}
 
 AppDataSource.initialize()
   .then(() => {
